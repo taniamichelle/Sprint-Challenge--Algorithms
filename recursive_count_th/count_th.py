11293406273a0f616745e9be573231729ddcb413
taniamichelle <tanislavens@yahoo.com>
2019-12-13 15:08:57 -0500

    completed algorithms questions

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -18,9 +18,9 @@ def count_th(word):
     s = len(string)
 
     if string not in word[0:s]:  # base case: if 'word' does not contain 'string' (which is equal to 'th'), return 0
-        return None
+        return 0
     elif string in word[0:s]:  # recursive function: if 'string'('th') is in 'word', add to occurrences list
-        occurrences.append[word]
+        occurrences.append(word)
     
     return count_th(word[s-1:])
 
