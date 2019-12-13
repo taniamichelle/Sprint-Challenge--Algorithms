de70b5bf38a98bbbdc33dfb32bf624ef9846a308
taniamichelle <tanislavens@yahoo.com>
2019-12-13 14:03:41 -0500

    added return statement to count function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -21,8 +21,9 @@ def count_th(word):
         return 0
     elif string in word[0:s] > 0:  # recursive function: if 'string'('th') is in 'word', add to occurrences list
         occurrences.append[word]
-        #     return counter(str1[n2-1:], str2 +1)
-        # return counter(str1[n2-1:], str2)
+    
+    return count_th(word[s-1:], string +1)
+
 
 # def count_th(word):
 #     string = 'th'
