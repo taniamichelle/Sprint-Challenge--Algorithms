7578bbbdfd80c4bea60e994c1fa8c691832b4ec0
taniamichelle <tanislavens@yahoo.com>
2019-12-13 14:14:29 -0500

    changed lines 21 and 25 in count file

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -18,11 +18,11 @@ def count_th(word):
     s = len(string)
 
     if string not in word[0:s]:  # base case: if 'word' does not contain 'string' (which is equal to 'th'), return 0
-        return 0
-    elif string in word[0:s] > 0:  # recursive function: if 'string'('th') is in 'word', add to occurrences list
+        return None
+    elif string in word[0:s]:  # recursive function: if 'string'('th') is in 'word', add to occurrences list
         occurrences.append[word]
     
-    return count_th(word[s-1:], string +1)
+    return count_th(word[s-1:])
 
 
 # def count_th(word):
